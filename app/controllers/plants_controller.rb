046a460482665842d1f5722bcf4711c8becb459a
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-02 19:09:00 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,10 +17,26 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    head :no_content
+  end
+ 
 
-  private
-
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
+def update
+  @plant = Plant.find(params[:id])
+  if @plant.update(plant_params)
+    render json: @plant
+  else
+    render json: { errors: @plant.errors.full_messages }, status: :unprocessable_entity
   end
 end
+
+private
+
+def plant_params
+  params.permit(:name, :image, :price, :is_in_stock)
+end
+end
+
